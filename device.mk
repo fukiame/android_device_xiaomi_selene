396ba30bfede97bb786614147c320e0c4703dd19
Orkun Ergün <orkun68100@gmail.com>
2023-10-08 14:59:35 +0000

    selene: Nuke Lineage Overlays
    * Move aux overlays to correct place
    * Add more package names to aux overlays
    
    Signed-off-by: Orkun Ergün <orkun68100@gmail.com>

--- device.mk
+++ device.mk
@@ -449,11 +449,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/mediatek
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay-lineage \
-    $(DEVICE_PATH)/overlay
-
 PRODUCT_PACKAGES += \
     ApertureOverlaySelene \
     ApertureQRScannerOverlaySelene \
